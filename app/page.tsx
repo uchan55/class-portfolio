b732181d12185c7760a9f59a80f1678165eda1c9
Kyung <uchan521@naver.com>
2024-05-23 13:02:24 +0900

    프로젝트 초기화 및 기본 컴포넌트 추가

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Layout } from "lucide-react";
+import Image from "next/image";
+import{layout} from "@/components/component/layout";
+
+export default function Home() {
+  return (
+    <Layout />
+  );
+}
