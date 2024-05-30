d42fa725f74f652805e5551ad1eeb35f0fb9f12c
uchan55 <162520808+uchan55@users.noreply.github.com>
2024-05-30 12:13:45 +0900

    Delete app directory

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Layout } from "lucide-react";
-import Image from "next/image";
-import{layout} from "@/components/component/layout";
-
-export default function Home() {
-  return (
-    <Layout />
-  );
-}
